e5e3349027ba8b2d6d785d78c08d820864674be4
Piyush Sharma <justpiyushsharma@gmil.com>
2022-11-29 23:17:49 +0530

    commiting over git, update requirements

--- preprocessor.py
+++ preprocessor.py
@@ -27,16 +27,18 @@ def preprocess (data):
         else: 
             users.append('group notification')
             messages.append(entry[0])
-    df['user']=users
-    df['message']=messages
-    df.drop(columns=['user_message'],inplace=True)
-    df['year']=df['date'].dt.year
-    df['month']=df['date'].dt.month_name()
-    df['day_name']= df['date'].dt.day_name()
-    df['month_num']= df['date'].dt.month
-    df['day']=df['date'].dt.day
-    df['hour']=df['date'].dt.hour
-    df['minute']=df['date'].dt.minute
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
     
     period = []
     for hour in df[['day_name', 'hour']]['hour']:
